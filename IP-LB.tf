274101dd5a4840b106e4b51f8df597863f3b564a
Arslan Saeed <arslan@hashicorp.com>
2019-05-29 10:07:52 -0400

    Added LB Rule

--- IP-LB.tf
+++ IP-LB.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "LoadBalancerPublicIP" {
   name                = "${var.prefix}-PublicIPForLB"
   location            = "${azurerm_resource_group.rsg.location}"
   resource_group_name = "${azurerm_resource_group.rsg.name}"
-  #public_ip_address_allocation = "static"
+  # public_ip_address_allocation = "static"
   allocation_method   = "Static"
 }
 
@@ -17,7 +17,15 @@ resource "azurerm_lb" "LoadBalancerWeb" {
     public_ip_address_id = "${azurerm_public_ip.LoadBalancerPublicIP.id}"
   }
 }
-
+resource "azurerm_lb_rule" "Web-RDP-Rule" {
+  resource_group_name            = "${azurerm_resource_group.test.name}"
+  loadbalancer_id                = "${azurerm_lb.test.id}"
+  name                           = "${var.prefix}-LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
 resource "azurerm_lb_backend_address_pool" "Web-LBBackendPool" {
   resource_group_name = "${azurerm_resource_group.rsg.name}"
   loadbalancer_id     = "${azurerm_lb.LoadBalancerWeb.id}"
